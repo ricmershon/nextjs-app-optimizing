7e3534f17ed432c5a4508eb49264d9008ae7d3d4
Ric Mershon <rmersh@ricmershon.com>
2024-10-28 18:22:12 -0400

    Use Next Image component for logo.

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,19 @@
-import logo from '@/assets/logo.png';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import logo from '@/assets/logo.png';
 
 export default function Header() {
+    console.log('^^^ LOGO ^^^', logo);
     return (
         <header id="main-header">
             <Link href="/">
-                <img src={logo.src} alt="Mobile phone with posts feed on it" />
+                <Image
+                    src={logo}
+                    width={100}
+                    height={100}
+                    alt="Mobile phone with posts feed on it"
+                />
             </Link>
             <nav>
                 <ul>
