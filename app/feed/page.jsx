8459dd7796c776f139c533bdb386c1a36d13662e
Ric Mershon <rmersh@ricmershon.com>
2024-10-29 17:15:51 -0400

    Dynamic metadata

--- app/feed/page.jsx
+++ app/feed/page.jsx
@@ -1,6 +1,16 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/data';
 
+// 'generateMetadata' is a Next reserved word
+export async function generateMetadata() {
+    const numPosts = (await getPosts()).length;
+
+    return {
+        title: 'Browse our Posts',
+        description: `Browse all of our ${numPosts} posts.`
+    }
+}
+
 export default async function FeedPage() {
     const posts = await getPosts();
     return (
